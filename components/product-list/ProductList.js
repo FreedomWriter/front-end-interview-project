62981538d5f06593ee7a025e53ecfdf8386513eb
FreedomWriter <natalienetworks@gmail.com>
2020-11-18 19:07:44 -0600

    added a loading spinner

--- components/product-list/ProductList.js
+++ components/product-list/ProductList.js
@@ -1,53 +1,11 @@
-// import React from "react";
-
-// // export default function ProductList() {
-// //   return (
-// //     <div>
-// //       <p>Product List commin in hot</p>
-// //     </div>
-// //   );
-// // }
-// // /
-
-// // products will be populated at build time by getStaticProps()
-// function ProductList({ products }) {
-//   console.log("from where I need em: ", products);
-//   return (
-//     <div>
-//       <p>Product List commin in hot</p>
-//     </div>
-//   );
-// }
-
-// // This function gets called at build time on server-side.
-// // It won't be called on client-side, so you can even do
-// // direct database queries. See the "Technical details" section.
-// export async function getStaticProps() {
-//   // Call an external API endpoint to get posts.
-//   // You can use any data fetching library
-//   const res = await fetch(
-//     "https://api.foxtrotchicago.com/v5/inventory/aisles/224/items?store_id=6"
-//   );
-//   const products = await res.json();
-//   console.log(products);
-
-//   // By returning { props: products }, the ProductList component
-//   // will receive `products` as a prop at build time
-//   return {
-//     props: {
-//       products,
-//     },
-//   };
-// }
-// // getStaticProps();
-
-// export default ProductList;
 import React, { useState, useEffect } from "react";
+import LoadingSpinner from "../loader/LoadingSpinner.component";
 
 function ProductList() {
   const [productGroups, setProductGroups] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+
   const fetchData = async () => {
     await fetch(
       "https://api.foxtrotchicago.com/v5/inventory/aisles/224/items?store_id=6"
@@ -73,7 +31,7 @@ function ProductList() {
     return <error>Something went wrong!</error>;
   }
   if (loading) {
-    return;
+    return <LoadingSpinner />;
   }
   return (
     <div>
